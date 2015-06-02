c41e5bfac892934e571516e6785c59dcb10924ca
root <root@ip-10-47-174-141.ec2.internal>
2015-06-02 20:14:27 +0000

    Committed by Udacity

---
+++ app/src/main/java/com/vesko/portfolio/portfolioapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.vesko.portfolio.portfolioapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when a button is clicked */
+    public void displayToast(View v) {
+        Log.d("vesko", "clicked");
+        String buttonText = v instanceof TextView ? ((TextView)v).getText().toString() : "";
+        String toastMessage = getString(R.string.this_button_will_launch_app, buttonText);
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
+    }
+}
